8762385f6421ef2919c47864a1e916a6c365bace
Klyxy Crimson <klyxycrimson@gmail.com>
2025-11-06 03:18:47 +0000

    make sure web screensize doesnt neglected, as well.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { CTA } from "@/components/landing/CTA";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen animate-in slide-in-from-left-12 duration-1000">
+    <div className="flex flex-col min-h-screen animate-in slide-in-from-left-full duration-700">
       <Header />
       <main className="flex-1">
         <Hero />
