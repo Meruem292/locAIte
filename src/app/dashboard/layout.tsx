24f8e16f0ab3bfc45d24f8abadf052b220e9ca67
Klyxy Crimson <klyxycrimson@gmail.com>
2025-11-13 04:25:48 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col bg-secondary/50">
+    <div className="min-h-screen flex flex-col bg-background">
         <DashboardHeader />
         <main className="flex-1 p-4 sm:p-6 md:p-8">
             {children}
