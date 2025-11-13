409378fdab72a25808ebbfa6cb23dee5bfb5aa16
Klyxy Crimson <klyxycrimson@gmail.com>
2025-11-13 03:55:15 +0000

    when showing the logo make sure it circle.

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 text-primary">
-      <Image src="/locaiteLogo.jpg" alt="LocAIte Logo" width={32} height={32} className="h-8 w-8" />
+      <Image src="/locaiteLogo.jpg" alt="LocAIte Logo" width={32} height={32} className="h-8 w-8 rounded-full" />
     </Link>
   );
 }
